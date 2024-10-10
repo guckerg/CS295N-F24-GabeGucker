208b8ca6f76a86af77eeb0057370c32adad1a485
gabe gucker <guckerg@my.lanecc.edu>
2024-10-09 20:58:26 -0700

    minor edits working towards lab02 from wednesday's lecture

---
+++ GGinfoSite/Controllers/About.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GGinfoSite.Controllers
+{
+    public class About : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Links()
+        {
+            return View();
+        }
+    }
+}
--- GGinfoSite/Controllers/HomeController.cs
+++ GGinfoSite/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace GGinfoSite.Controllers
             return View();
         }
 
+        public IActionResult MoreInfo()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
