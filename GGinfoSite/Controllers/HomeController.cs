f149d4e8794e840f1252a660d7135eb3301ed88b
gabe gucker <guckerg@my.lanecc.edu>
2024-10-16 10:09:23 -0700

    Removed privacy view and dependancies. Added missing navigation to blog/about

--- GGinfoSite/Controllers/HomeController.cs
+++ GGinfoSite/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace GGinfoSite.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         public IActionResult Overview()
         {
             return View();
