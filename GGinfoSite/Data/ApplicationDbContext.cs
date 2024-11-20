98f78eda394d69e142b66f41a71842e3e515a03f
guckerg <guckerg@my.lanecc.edu>
2024-11-19 19:06:03 -0800

    Pushing to enable viewing of Lab06 progress for class code review. Not complete.

---
+++ GGinfoSite/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GGinfoSite.Models;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.EntityFrameworkCore;
+
+namespace GGinfoSite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // constructor just calls the base class constructor
+        public ApplicationDbContext(
+           DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        // one DbSet for each domain model class
+        public DbSet<BlogPost> BlogPost { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
