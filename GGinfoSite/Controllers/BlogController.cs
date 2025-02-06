f0ebdb27ebdc4d54f83753c70d2e6aa4b59e23cb
Gabe Gucker <guckerg.my.lanecc.edu>
2025-02-05 23:36:13 -0800

    Refactored lots of methods to be async compatible. Tests need to be fixed.

--- GGinfoSite/Controllers/BlogController.cs
+++ GGinfoSite/Controllers/BlogController.cs
@@ -21,18 +21,17 @@ namespace GGinfoSite.Controllers
             repo = r;
         }
         
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var blogPosts = repo.GetBlogPosts();
+            var blogPosts = await repo.GetBlogPostsAsync();
             return View(blogPosts);
         }
 
-        public IActionResult Filter(string poster, string date)
+        public async Task<IActionResult> Filter(string poster, string date)
         {
-            var blogPosts = repo.GetBlogPosts()
-                .Where(b => b.Poster.UserName == poster || poster == null)
-                .ToList();
-            return View("Index", blogPosts);
+            var blogPosts = await repo.GetBlogPostsAsync();
+            var filteredBlogPosts = blogPosts.Where(b => b.Poster.UserName == poster || poster == null).ToList();
+            return View("Index", filteredBlogPosts);
         }
 
         public IActionResult About()
@@ -40,15 +39,15 @@ namespace GGinfoSite.Controllers
             return View();
         }
 
-        public IActionResult BlogPost() //make async
+        public IActionResult BlogPost()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult BlogPost(BlogPost model) //make async
+        public async Task<IActionResult> BlogPostAsync(BlogPost model)
         {
-            model.Poster = userManager.GetUserAsync(User).Result;
+            model.Poster = await userManager.GetUserAsync(User);
             model.PostTime = DateTime.Now;
 
             repo.StoreBlogPost(model);
